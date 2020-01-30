ac3e5dc6b41047b018fc0ce221cc085e696c6298
Kiran <kiranlali99@gmail.com>
2020-01-30 12:18:36 +0530

    Throwing dynamic error message

---
+++ ios/qrcodescanner/ViewControllerPolicy.h
@@ -0,0 +1,12 @@
+//
+//  ViewControllerPolicy.h
+//  VeridiumExportEnrollDemo
+//
+//  Copyright © 2018 Veridium IP Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerPolicy : UIViewController
+
+@end
